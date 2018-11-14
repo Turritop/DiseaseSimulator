5dc806b0411d9335c68d37db2f246cdcd701a759
Turritop <35125610+Turritop@users.noreply.github.com>
2018-11-14 11:02:42 -0600

    Add files via upload

--- Human.java
+++ Human.java
@@ -2,12 +2,14 @@ import java.awt.*;
 
 public class Human {
 
+    public String disease;
     public boolean alive;
     public boolean infected;
     public boolean immune;
     public int roundsInfected;
 
-    public Human() {
+    public Human(String disease) {
+        this.disease = disease;
         infected = false;
         immune = false;
         alive = true;
@@ -32,13 +34,56 @@ public class Human {
         }
     }
 
-    public void kill() {
+    private void kill() {
         alive = false;
     }
 
-    public void cure() {
+    private void cure() {
         infected = false;
         immune = true;
+    }
+
+    public boolean attemptToCure() {
+        if (!infected) {
+            return false;
+        }
+        //the chance of getting over an infection is an exponential random variable with given mean
+        //F(x) = 1 - e^(-x/mean)
+        double mean = Main.diseaseMap.get(disease)[0];
+        double chanceToCure = 1 - Math.pow(Math.E, -1 * roundsInfected / mean); //will always be between 1 and 0
+        if (Math.random() < chanceToCure) {
+            cure();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean attemptToKill() {
+        if (!infected) {
+            return false;
+        }
+        //the chance of dying from an infection is an exponential random variable with given mean
+        //F(x) = 1 - e^(-x/mean)
+        double mean = Main.diseaseMap.get(disease)[1];
+        double chanceToKill = 1 - Math.pow(Math.E, -1 * roundsInfected / mean); //will always be between 1 and 0
+        if (Math.random() < chanceToKill) {
+            kill();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean attemptToInfect() {
+        if (!infected) {
+            return false;
+        }
+        //each disease has a static chance to infect a neighboring person
+        double chanceToInfect = Main.diseaseMap.get(disease)[3];
+        if (Math.random() < chanceToInfect) {
+            infect();
+            return true;
+        }
+        return false;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,47 +1,89 @@
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-    public static final int DRAWING_PANEL_WIDTH = 500;
-    public static final int DRAWING_PANEL_HEIGHT = 250;
-    public static final int NUM_HUMANS_WIDTH = 30;
-    public static final int NUM_HUMANS_HEIGHT = 15;
-    public static final double WAIT_TIMER = 100;
+    private static final int DRAWING_PANEL_WIDTH = 500;
+    private static final int DRAWING_PANEL_HEIGHT = 250;
+    private static final int NUM_HUMANS_WIDTH = 30;
+    private static final int NUM_HUMANS_HEIGHT = 15;
 
-    public static int roundsBeforeDiseaseCanKill;
-    public static int roundsBeforeDiseaseCure = 3;
-    public static double infectionRate;
-    public static double killRate;
-    public static double cureRate;
-
+    public static Map<String, double[]> diseaseMap;
+    //the array goes: meanTimeUntilCure, meanTimeUntilDeath, infectionRate
+    public static String diseaseChoice;
+    private static double waitTimer = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Disease Simulator\n");
         Scanner sc = new Scanner(System.in);
+        diseaseMap = new HashMap<>();
         designDisease(sc);
         runSimulation(sc);
     }
 
     public static void designDisease(Scanner sc) {
-        System.out.println("Design your disease:");
-        System.out.println("What is the chance of infection (0-1)?");
-        double input = sc.nextDouble();
-        if (input == 42) {
-            roundsBeforeDiseaseCanKill = 2 * 24;
-            infectionRate = .5;
-            killRate = .2;
-            cureRate = .1;
-        } else {
-            infectionRate = input;
-            System.out.println("How many hours must someone be sick before they can die from the disease?");
-            roundsBeforeDiseaseCanKill = sc.nextInt();
-            System.out.println("What is the chance of someone dying once they've reached that point (0-1)?");
-            killRate = sc.nextDouble();
-            System.out.println("What is the chance of a sick person getting better within the next hour (0-1)?");
-            cureRate = sc.nextDouble();
+        System.out.println("Do you want to design your own disease? Y/N");
+        String next = sc.next();
+        if (next.toLowerCase().contains("y")) {
+            double[] parameters = new double[3];
+            System.out.println();
+            System.out.println("On average, how many days will someone be sick before they die from the disease?");
+            double input = sc.nextDouble();
+            parameters[1] = input * 24;
+            System.out.println("On average, how many days will someone be sick before they get over the disease?");
+            parameters[0] = sc.nextInt() * 24;
+            System.out.println("How likely is it for someone to get infected by a neighboring sick person? (0-1)");
+            input = sc.nextDouble();
+            while (input > 1 || input < 0) {
+                System.out.println("Invalid input: " + input);
+                System.out.println("How likely is it for someone to get infected by a neighboring sick person? (0-1)");
+                input = sc.nextDouble();
+            }
+            parameters[2] = input;
+            diseaseMap.put("Custom", parameters);
+            diseaseChoice = "Custom";
             System.out.println("\nCongratulations on designing your disease.");
+        } else {
+            implementDiseaseMap();
+            System.out.println("Choose your disease \nType 1 for the flu, 2 for the bubonic plague, 3 for tuberculosis, 4 for ebola");
+            int choice = sc.nextInt();
+            while (choice <= 0 || choice > 4) {
+                System.out.println("Invalid input: " + choice);
+                System.out.println("Choose your disease \nType 1 for the flu, 2 for the bubonic plague, 3 for tuberculosis, 4 for ebola");
+                choice = sc.nextInt();
+            }
+            if (choice == 1) {
+                diseaseChoice = "Flu";
+            } else if (choice == 2) {
+                diseaseChoice = "Plague";
+            } else if (choice == 3) {
+                diseaseChoice = "Tuberculosis";
+                waitTimer = 1;
+            } else {
+                diseaseChoice = "Ebola";
+                waitTimer = 50;
+            }
         }
+
+    }
+
+    public static void implementDiseaseMap() {
+        String[] key1 = {"Flu", "Plague", "Tuberculosis", "Ebola"};
+        //meanTimeUntilCure, meanTimeUntilDeath, infectionRate
+        //sources: https://www.cdc.gov/flu/professionals/acip/2018-2019/background/background-epidemiology.htm
+        double[] fluValues = {7 * 24, 14 * 24, .08};
+        diseaseMap.put("Flu", fluValues);
+        //sources: http://www.who.int/csr/resources/publications/plague/whocdscsredc992a.pdf  https://en.wikipedia.org/wiki/Bubonic_plague
+        double[] plagueValues = {30, 24, .40};
+        diseaseMap.put("Plague", plagueValues);
+        //sources: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4235436/  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3070694/
+        double[] tbValues = {7 * 24 * 52 * 13, 7 * 24 * 52 * 7, .1};
+        diseaseMap.put("Tuberculosis", tbValues);
+        //sources: http://www.who.int/news-room/fact-sheets/detail/ebola-virus-disease  https://en.wikipedia.org/wiki/Ebola_virus_disease
+        double[] ebolaValues = {7 * 24 * 14, 7 * 24 * 12, .75};
+        diseaseMap.put("Ebola", ebolaValues);
     }
 
     public static void runSimulation(Scanner sc) {
@@ -56,7 +98,6 @@ public class Main {
         int killCounter = 0;
         while (keepGoing) {
             drawHumans(population, g);
-
             keepGoing = false;
             for (int r = 1; r <= NUM_HUMANS_WIDTH; r++) {
                 for (int c = 1; c <= NUM_HUMANS_HEIGHT; c++) {
@@ -64,12 +105,9 @@ public class Main {
                     if (currentHuman.infected) { //only keep going when at least one person is infected
                         keepGoing = true;
                         currentHuman.roundsInfected++; // need to keep track of this
-                        infectNeighbors(r, c, population, infectionRate);
-                        if (Math.random() < cureRate && currentHuman.roundsInfected >= roundsBeforeDiseaseCure) {
-                            currentHuman.cure();
-                        }
-                        else if (currentHuman.roundsInfected >= roundsBeforeDiseaseCanKill && Math.random() < killRate && currentHuman.alive) {
-                            currentHuman.kill();
+                        infectNeighbors(r, c, population);
+                        currentHuman.attemptToCure();
+                        if (currentHuman.attemptToKill()) {
                             killCounter++;
                         }
                     }
@@ -78,7 +116,7 @@ public class Main {
             }
 
             double currentTime = System.currentTimeMillis();
-            while (System.currentTimeMillis() < (currentTime + WAIT_TIMER)) {
+            while (System.currentTimeMillis() < (currentTime + waitTimer)) {
             }
             roundCounter++;
         }
@@ -100,7 +138,7 @@ public class Main {
         int patientZeroX = (int) (Math.random() * NUM_HUMANS_HEIGHT) + 1;
         for (int r = 0; r < population.length; r++) {
             for (int c = 0; c < population[0].length; c++) {
-                population[r][c] = new Human();
+                population[r][c] = new Human(diseaseChoice);
                 if (r == patientZeroX && c == patientZeroY) {
                     population[r][c].infect();
                 }
@@ -108,13 +146,12 @@ public class Main {
         }
     }
 
-    public static void infectNeighbors(int row, int col, Human[][] population, double infectionRate) {
+    public static void infectNeighbors(int row, int col, Human[][] population) {
         for(int r = row - 1; r <= row + 1; r++) {
             for(int c = col - 1; c <= col + 1; c++) {
-                if ((r != row || c != col) //can't re-infect the original human
-                        && Math.random() < infectionRate) {
+                if ((r != row || c != col)) {
                     Human h = population[r][c];
-                    h.infect();
+                    h.attemptToInfect();
                 }
             }
         }
